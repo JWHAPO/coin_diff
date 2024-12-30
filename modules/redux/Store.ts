a5e04dbb257adb4a318e06f8d1c472b8672e556e
YJLEE <dldbwjd24@gmail.com>
2024-12-30 13:29:26 +0900

    feat : create store/rootReducer

---
+++ modules/redux/Store.ts
@@ -0,0 +1,23 @@
+import {configureStore} from '@reduxjs/toolkit';
+import RootReducer, {RootState} from './RootReducer';
+import {persistStore, persistReducer, PersistConfig} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import logger from 'redux-logger';
+
+const persistConfig: PersistConfig<RootState> = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: [], //저장하지 않을 reducer
+};
+
+export const Store = configureStore({
+  reducer: persistReducer(persistConfig, RootReducer)!,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger),
+});
+
+export const Persistor = persistStore(Store);
+
+export type AppDispatch = typeof Store.dispatch;
