aeaff8d839ef407c154ef48b567c7e675a42551e
YJLEE <dldbwjd24@gmail.com>
2024-12-30 13:29:55 +0900

    feat : redux sample run
    feat : redux sample run

--- App.tsx
+++ App.tsx
@@ -7,16 +7,44 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  Button,
   Text,
   View,
+  TextInput,
 } from 'react-native';
+import {RootState} from './modules/redux/RootReducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {setNickName, setUser} from './modules/redux/slice/UserSlice';
+import {User} from './types/users/UserType';
 
 function App(): React.JSX.Element {
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state: RootState) => state.user);
+
+  const setUserInfo = () => {
+    const userInfo: User = {
+      id: '123',
+      nickName: text,
+      phoneNumber: '010-1234-5678',
+    };
+
+    const result = dispatch(setUser(userInfo));
+  };
+
+  function checkUserInfo() {
+    setNickName(userInfo.nickName);
+  }
+
+  const [text, setText] = React.useState('');
+  const [nickName, setNickName] = React.useState('');
 
   return (
-    <SafeAreaView >
+    <SafeAreaView>
       <View>
-        <Text> Page 1 </Text>
+        <TextInput style={styles.input} onChangeText={setText} value={text} />
+        <Button title="닉네임 저장하기" onPress={setUserInfo} />
+        <Text>{nickName}</Text>
+        <Button title="닉네임 확인하기" onPress={checkUserInfo} />
       </View>
     </SafeAreaView>
   );
@@ -38,6 +66,12 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
