3f2bc98e9d3300cb0fcdd90625d71cc8a1c7f717
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-12-31 08:41:50 +0900

    버튼 위치 & 디자인 수정

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -10,7 +10,9 @@ const DetailsScreen = ({ route, navigation }: { route: any; navigation: any }) =
     <SafeAreaView style={styles.container}>
       <View style={styles.inner}>
         <Text>상세 화면 {detail}의 내용</Text>
-        <Button title="홈으로 돌아가기" onPress={() => navigation.goBack()} />
+        <View style={styles.buttonContainer}>
+          <Button title="홈으로 돌아가기" onPress={() => navigation.goBack()} />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -24,6 +26,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: '80%',
   },
 });
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,18 +8,24 @@ const HomeScreen = ({ navigation }: { navigation: any }) => {
     <SafeAreaView style={styles.container}>
       <View style={styles.inner}>
         <Text>홈 화면</Text>
-        <Button
-          title="상세 화면 1로 이동"
-          onPress={() => navigation.navigate('Details', { detail: '1' })}
-        />
-        <Button
-          title="상세 화면 2로 이동"
-          onPress={() => navigation.navigate('Details', { detail: '2' })}
-        />
-        <Button
-          title="상세 화면 3로 이동"
-          onPress={() => navigation.navigate('Details', { detail: '3' })}
-        />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="상세 화면 1로 이동"
+            onPress={() => navigation.navigate('Details', { detail: '1' })}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="상세 화면 2로 이동"
+            onPress={() => navigation.navigate('Details', { detail: '2' })}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="상세 화면 3로 이동"
+            onPress={() => navigation.navigate('Details', { detail: '3' })}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,6 +39,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: '80%',
   },
 });
 
